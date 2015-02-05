0b0e8a1070bcaf8198876eb5b96881b66498bfa7
Mrvon <mrvon@qq.com>
2015-02-06 00:51:50 +0800

    add some comment

--- lobject.h
+++ lobject.h
@@ -560,13 +560,13 @@ typedef struct Node {
 typedef struct Table {
 	CommonHeader;
 	lu_byte flags;  /* 1<<p means tagmethod(p) is not present */
-	lu_byte lsizenode;  /* log2 of size of `node' array */
+	lu_byte lsizenode;  /* log2 of size of `node' array (hash part)*/
 	struct Table *metatable;
-	TValue *array;  /* array part */
-	Node *node;
-	Node *lastfree;  /* any free position is before this position */
+	TValue *array;  /* (array part) */
+    Node *node;     /* (hash part) */
+	Node *lastfree;  /* any free position is before this position (hash part) */
 	GCObject *gclist;
-	int sizearray;  /* size of `array' array */
+	int sizearray;  /* size of `array' (array part) */
 } Table;
 
 
