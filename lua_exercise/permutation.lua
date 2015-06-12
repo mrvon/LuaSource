eeafaf1b14a2a3fd58a4f74768729beba8a617c2
Dennis <mrvon@qq.com>
2015-06-12 17:13:38 +0800

    permutation

--- lua_exercise/permutation.lua
+++ lua_exercise/permutation.lua
@@ -20,4 +20,20 @@ function printResult(a)
     io.write("\n")
 end
 
-permgen({1, 2, 3, 4})
+-- permgen({1, 2, 3, 4})
+
+function permutations(a)
+    local co = coroutine.create(function () permgen(a) end)
+    return function () -- iterator
+        local code, res = coroutine.resume(co)
+        return res
+    end
+end
+
+function permutations(a)
+    return coroutine.wrap(function () permgen(a) end)
+end
+
+for p in permutations{"a", "b", "c"} do
+    printResult(p)
+end
