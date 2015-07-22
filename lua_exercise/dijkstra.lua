0a3b3094a0a052e9be036af1df4b774e71b4a209
Dennis <mrvon@qq.com>
2015-07-22 10:53:51 +0800

    dijkstra

--- lua_exercise/dijkstra.lua
+++ lua_exercise/dijkstra.lua
@@ -23,12 +23,12 @@ local function readgraph()
 end
 
 
-local function extract_min(Q, D)
+local function extract_min(OpenSet, Dist)
     local min_len = nil
     local min_index = nil
-    for i = 1, #Q do
-        local node = Q[i]
-        local len = D[node]
+    for i = 1, #OpenSet do
+        local node = OpenSet[i]
+        local len = Dist[node]
         if len then
             if min_len == nil or len < min_len then
                 min_len = len
@@ -36,85 +36,88 @@ local function extract_min(Q, D)
             end
         end
     end
-    local temp = Q[min_index]
-    table.remove(Q, min_index)
+    local temp = OpenSet[min_index]
+    table.remove(OpenSet, min_index)
     return temp
 end
 
-local function dijkstra(g, s, w)
-    local D = {}
-    local S = {}
-    local Q = {}
-    local P = {}
+local function dijkstra(Graph, source, target)
+    local Dist     = {} -- Distance from node to source
+    local CloseSet = {}
+    local OpenSet  = {}
+    local Prev     = {} -- Previous node in optimal path
 
     local i = 1
-    for _, node in pairs(g) do
-        Q[i] = node
-        D[node] = math.huge
+    for _, node in pairs(Graph) do
+        OpenSet[i] = node
+        Dist[node] = math.huge
         i = i + 1
     end
-    D[s] = 0
+    Dist[source] = 0
 
-    while #Q > 0 do
-        local u = extract_min(Q, D)
-        table.insert(S, u)
+    while #OpenSet > 0 do
+        local u = extract_min(OpenSet, Dist)
+        table.insert(CloseSet, u)
         for v, d in pairs(u.adj) do
-            if D[v] > D[u] + d then
-                D[v] = D[u] + d
-                P[v] = u
+            if Dist[v] > Dist[u] + d then
+                Dist[v] = Dist[u] + d
+                Prev[v] = u
             end
         end
     end
 
-    printpath(P, w, D)
+    printpath(Prev, target, Dist)
 end
 
-local function breadth_first_find_path(g, s, w)
-    local visited = {}
-    local Q = {}
-    local D = {}
-    local P = {}
-    table.insert(Q, s)
+local function breadth_first_find_path(Graph, source, target)
+    local CloseSet = {}
+    local OpenSet  = {}
+    local Dist     = {}
+    local Prev     = {}
+
+    table.insert(OpenSet, source)
 
     local i = 1
-    for _, node in pairs(g) do
-        D[node] = math.huge
+    for _, node in pairs(Graph) do
+        Dist[node] = math.huge
         i = i + 1
     end
-    D[s] = 0
+    Dist[source] = 0
 
-    while #Q > 0 do
-        local u = Q[1]
-        table.remove(Q, 1)
+    while #OpenSet > 0 do
+        local u = OpenSet[1]
+        table.remove(OpenSet, 1)
 
         for v, d in pairs(u.adj) do
-            if D[v] > D[u] + d then
-                D[v] = D[u] + d
-                P[v] = u
+            if Dist[v] > Dist[u] + d then
+                Dist[v] = Dist[u] + d
+                Prev[v] = u
             end
 
-            if not visited[v] then
-                visited[v] = true
-                table.insert(Q, v)
+            if not CloseSet[v] then
+                CloseSet[v] = true
+                table.insert(OpenSet, v)
             end
         end
     end
 
-    printpath(P, w, D)
+    printpath(Prev, target, Dist)
 end
 
-function printpath(P, w, D)
-    print("path length: " .. D[w])
-    while P[w] do
-        io.write(w.name .. " ")
-        w = P[w]
+function printpath(Prev, target, Dist)
+    print("path length: " .. Dist[target])
+
+    while Prev[target] do
+        io.write(target.name .. " ")
+        target = Prev[target]
     end
-    print(w.name)
+
+    print(target.name)
 end
 
-g = readgraph()
-s = name2node(g, "a")
-w = name2node(g, "b")
+Graph = readgraph()
+source = name2node(Graph, "a")
+target = name2node(Graph, "b")
 
-dijkstra(g, s, w)
-breadth_first_find_path(g, s, w)
+dijkstra(Graph, source, target)
+breadth_first_find_path(Graph, source, target)
