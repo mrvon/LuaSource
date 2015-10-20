2e7dfa313c85f290c94eab49fa29bc2f9910e9c1
dennis <mrvon@qq.com>
2015-10-20 16:41:16 +0800

    class

--- lua_exercise/class.lua
+++ lua_exercise/class.lua
@@ -1,5 +1,5 @@
 function my_print(str, deep)
-    print(string.rep(" ", deep * 9) .. str)
+    -- print(string.rep(" ", deep * 9) .. str)
 end
 
 function clone(object)
@@ -48,4 +48,69 @@ assert(rc.sub.sub2.name == base.sub.sub2.name)
 
 
 function class(class_name, super)
+    local super_type = type(super)
+    local sub_class
+
+    if super_type ~= "function" and super_type ~= "table" then
+        super_type = nil
+        super = nil
+    end
+
+    if super_type == "function" or (super and super.__ctype == 1) then
+
+        -- super is C++ object
+        
+        sub_class = {}
+
+        if super_type == "table" then
+            -- copy fields from super
+            for key, value in pairs(super) do
+                sub_class[key] = value
+            end
+            sub_class.__create = super.__create
+            sub_class.super = super
+        else
+            sub_class.__create = super
+        end
+
+        sub_class.ctor = function() end
+        sub_class.__cname = class_name
+        sub_class.__ctype = 1
+
+        function sub_class.new(...)
+            local instance = sub_class.__create(...)
+            -- copy fields from class to native object
+            for key, value in pairs(sub_class) do
+                instance[key] = value
+            end
+            instance.class = sub_class
+            instance.ctor(instance, ...)
+            return instance
+        end
+    else
+        
+        -- super is Lua object
+
+        if super then
+            sub_class = clone(super)
+            sub_class.super = super
+        else
+            sub_class = {
+                ctor = function() end
+            }
+        end
+
+        sub_class.__cname = class_name
+        sub_class.__ctype = 2
+        sub_class.__index = sub_class
+
+        function sub_class.new(...)
+            local instance = setmetatable({}, sub_class)
+            instance.class = sub_class
+            instance.ctor(instance, ...)
+            return instance
+        end
+    end
+
+    return sub_class
 end
