f9f102aea2c8caec8020304cd913d5baabbb35a4
dennis <mrvon@qq.com>
2015-10-20 17:53:46 +0800

    class

--- lua_exercise/class.lua
+++ lua_exercise/class.lua
@@ -1,24 +1,24 @@
-function my_print(str, deep)
-    print(string.rep(" ", deep * 9) .. str)
+function debug_print(str, deep)
+    -- print(string.rep(" ", deep * 9) .. str)
 end
 
 function clone(object)
     local lookup_table = {}
     local function __copy(object, deep)
         if type(object) ~= "table" then
-            my_print("Case 1: " .. tostring(object), deep)
+            debug_print("Case 1: " .. tostring(object), deep)
             return object
         elseif lookup_table[object] then
-            my_print("Case 2", deep)
+            debug_print("Case 2: " .. tostring(lookup_table[object]), deep)
             return lookup_table[object]
         end
-        my_print("-------- Case 3 start --------", deep)
+        debug_print("-------- Case 3 start --------", deep)
         local new_table = {}
         lookup_table[object] = new_table
         for key, value in pairs(object) do
             new_table[__copy(key, deep + 1)] = __copy(value, deep + 1)
         end
-        my_print("-------- Case 3 end ----------", deep)
+        debug_print("-------- Case 3 end ----------", deep)
         return setmetatable(new_table, getmetatable(object))
     end
     return __copy(object, 0)
@@ -59,6 +59,8 @@ function class(class_name, super)
     if super_type == "function" or (super and super.__ctype == 1) then
 
         -- super is C++ object
+        -- unuse right not
+        assert(false)
         
         sub_class = {}
 
