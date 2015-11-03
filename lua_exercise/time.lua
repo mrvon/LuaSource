822a1e7eaaab41d86e0bd053c52ac74d394191ee
dennis <mrvon@qq.com>
2015-11-03 16:06:48 +0800

    day diff

--- lua_exercise/time.lua
+++ lua_exercise/time.lua
@@ -23,3 +23,22 @@ print(
     date.min,
     date.sec
 )
+
+function day_diff(time_x, time_y)
+    if time_x < time_y then
+        time_x, time_y = time_y, time_x
+    end
+
+    local date_x = os.date("*t", time_x)
+    local date_y = os.date("*t", time_y)
+
+    date_x.hour = 0
+    date_x.min = 0
+    date_x.sec = 0
+    date_y.hour = 0
+    date_y.min = 0
+    date_y.sec = 0
+
+    local diff_time = os.time(date_x) - os.time(date_y)
+    return diff_time / (3600 * 24)
+end
