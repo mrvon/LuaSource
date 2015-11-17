2d93e5387ef59ad05e199c56797e928a2788d79f
dennis <mrvon@qq.com>
2015-11-17 19:28:49 +0800

    seri

--- lua_exercise/serialization.lua
+++ lua_exercise/serialization.lua
@@ -17,20 +17,6 @@ assert(isvalidindentfier("__ab1c3"))
 assert(not isvalidindentfier("1a"))
 assert(not isvalidindentfier("$a"))
 
-function quote(s)
-    -- find maximum length of sequence of equal signs
-    local n = -1
-    for w in string.gmatch(s, "]=*]") do
-        n = math.max(n, #w - 2) -- -2 to remove the ']'s
-    end
-
-    -- produce a string with 'n' plus one equal signs
-    local eq = string.rep("=", n + 1)
-
-    -- build quoted string
-    return string.format("[%s[%s]%s]", eq, s, eq)
-end
--- print(quote("[===[hello world [[Nest]] ]===]"))
 
 function indent(out_arr, indent_level)
     for i = 1, indent_level do
