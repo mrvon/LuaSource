68997fc8892085ca1716e18289574e73c88ef8a7
dennis <mrvon@qq.com>
2015-11-18 20:11:29 +0800

    generic_for

---
+++ lua_exercise/generic_for.lua
@@ -0,0 +1,30 @@
+--------------------------------------------------------------------------------
+function generic_for(_f, _s, _var, block_callback)
+    while true do
+        local t = table.pack(_f(_s, _var))
+        _var = t[1]
+
+        if _var == nil then
+            break
+        end
+
+        block_callback(table.unpack(t))
+    end
+end
+--------------------------------------------------------------------------------
+
+local t = {
+    Name = "skynet",
+    Author = "Cloudwu",
+}
+
+generic_for(next, t, nil, function(...)
+    print(...)
+end)
+
+
+--------------------------------------------------------------------------------
+-- Simple way
+for k, v in next, t, nil do
+    print(k, v)
+end
