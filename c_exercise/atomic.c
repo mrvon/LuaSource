f4095e8762e56af2a7bb7b919bf94cc6ddf2f367
dennis <mrvon@qq.com>
2015-12-02 16:42:42 +0800

    atomic

--- c_exercise/atomic.c
+++ c_exercise/atomic.c
@@ -1,9 +1,41 @@
-#define ATOM_CAS(ptr, oval, nval) __sync_bool_compare_and_swap(ptr, oval, nval)
-#define ATOM_CAS_POINTER(ptr, oval, nval) __sync_bool_compare_and_swap(ptr, oval, nval)
-#define ATOM_INC(ptr) __sync_add_and_fetch(ptr, 1)
-#define ATOM_FINC(ptr) __sync_fetch_and_add(ptr, 1)
-#define ATOM_DEC(ptr) __sync_sub_and_fetch(ptr, 1)
-#define ATOM_FDEC(ptr) __sync_fetch_and_sub(ptr, 1)
-#define ATOM_ADD(ptr,n) __sync_add_and_fetch(ptr, n)
-#define ATOM_SUB(ptr,n) __sync_sub_and_fetch(ptr, n)
-#define ATOM_AND(ptr,n) __sync_and_and_fetch(ptr, n)
+#include <stdio.h>
+#include "atomic.h"
+
+int main(int argc, char const* argv[])
+{
+    unsigned int k = 0;
+
+    printf("K = %u\n", k);
+
+    printf("Inc and fetch %u\n", ATOM_INC(&k));
+    printf("Dec and fetch %u\n", ATOM_DEC(&k));
+
+    printf("K = %u\n", k);
+
+    printf("Fetch and inc %u\n", ATOM_FINC(&k));
+    printf("Fetch and dec %u\n", ATOM_FDEC(&k));
+
+    printf("K = %u\n", k);
+
+    printf("Add and fetch %u\n", ATOM_ADD(&k, 1024));
+    printf("Sub and fetch %u\n", ATOM_SUB(&k, 1024));
+
+    printf("K = %u\n", k);
+
+    printf("Add and fetch %u\n", ATOM_ADD(&k, 0xff));
+    printf("And and fetch %u\n", ATOM_AND(&k, 0x0));
+    printf("And and fetch %u\n", ATOM_OR(&k, 0xf));
+
+    printf("K = %u\n", k);
+
+    if (ATOM_CAS(&k, 0, 1)) {
+        printf("Atom compare and swap ok!\n");
+    }
+    else {
+        printf("Atom compare and swap failed!\n");
+    }
+
+    printf("K = %u\n", k);
+
+    return 0;
+}
