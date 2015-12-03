2396a560e84dbf5688caced31d7bb39277291536
dennis <mrvon@qq.com>
2015-12-03 12:02:48 +0800

    offsetof

--- c_exercise/test.c
+++ c_exercise/test.c
@@ -1,25 +1,14 @@
 #include "test.h"
-
-typedef struct Foo {
-	int n;
-	char c;
-	double f;
-	int n2;
-} Foo;
+#include <stdio.h>
 
 int main(void) {
-	//test_conf();
-    //test_newconf();
+    /* test_conf(); */
+    /* test_newconf(); */
     simple_interpreter();
-	//test_reg();
-	//test_stack();
-    //test_push();
+    /* test_reg(); */
+    /* test_stack(); */
+    /* test_push(); */
 
-	//fprintf(stdout, "INT: %d\n", offsetof(Foo, n));
-	//fprintf(stdout, "CHAR: %d\n", offsetof(Foo, c));
-	//fprintf(stdout, "DOUBLE: %d\n", offsetof(Foo, f));
-	//fprintf(stdout, "INT: %d\n", offsetof(Foo, n2));
-
-	getchar();
+    getchar();
 	return 0;
 }
