3fef1b744b4b726958f3d9ddbf6bd211e2ec2322
dennis <mrvon@qq.com>
2016-02-03 17:53:00 +0800

    update

--- lua_exercise/utf8.lua
+++ lua_exercise/utf8.lua
@@ -1,5 +1,16 @@
 -- utf8
 
-local code_point = utf8.codepoint("€")
+local utf8_char = "€"
+local utf8_char_list = "€€€€"
+
+local code_point = utf8.codepoint(utf8_char)
 print(code_point)
 print(utf8.char(code_point))
+
+-- utf8.charpattern
+
+for p, c in utf8.codes(utf8_char_list) do
+    print("Position:", p, "CodePoint:", c)
+end
+
+print("Len of char list", utf8.len(utf8_char_list))
