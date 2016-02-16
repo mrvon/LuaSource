1147d7b96dc4d388d1b71581654fb53b923ced41
dennis <mrvon@qq.com>
2016-02-16 17:43:41 +0800

    parser

---
+++ c_exercise/parser.cc
@@ -0,0 +1,257 @@
+#include <cassert>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+// The Lexer returns tokes [0-255] if it is an unknown character, otherwise one
+// of these for known things.
+enum Token {
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5,
+};
+
+static std::string IdentifierStr;   // Filled in if tok_identifier
+static double NumVal;               // Filled in if tok_number
+
+// gettok - Return the next token from standard input.
+static int gettok() {
+    static int LastChar = ' ';
+
+    // Skip any whitespace.
+    while (isspace(LastChar)) {
+        LastChar = getchar();
+    }
+
+    if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+        IdentifierStr = LastChar;
+
+        while (isalnum(LastChar = getchar())) {
+            IdentifierStr += LastChar;
+        }
+
+        if (IdentifierStr == "def") {
+            return tok_def;
+        }
+        if (IdentifierStr == "extern") {
+            return tok_extern;
+        }
+
+        return tok_identifier;
+    }
+
+    if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
+        std::string NumStr;
+
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    if (LastChar == '#') {
+        // Comment until end of line.
+        do {
+            LastChar = getchar();
+        } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF) {
+            return gettok();
+        }
+    }
+
+    // Check for end of file, Don't eat the EOF.
+    if (LastChar == EOF) {
+        return tok_eof;
+    }
+
+    // Otherwise, just return the character as its ascii value.
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
+
+/// ExprAST - Base class for all expression nodes.
+class ExprAST {
+public:
+    virtual ~ExprAST() {}
+};
+
+/// NumberExprAST - Expression class for numeric literals like "1.0"
+class NumberExprAST : public ExprAST {
+public:
+    NumberExprAST(double Val) : Val(Val) {}
+private:
+    double Val;
+};
+
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+public:
+    VariableExprAST(const std::string &Name) : Name(Name) {}
+private:
+    std::string Name;
+};
+
+/// BinaryExprAST - Expression class for a binary operator.
+class BinaryExprAST : public ExprAST {
+public:
+    BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS,
+            std::unique_ptr<ExprAST> RHS)
+        : Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+private:
+    char Op;
+    std::unique_ptr<ExprAST> LHS, RHS;
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+public:
+    CallExprAST(const std::string &Callee,
+            std::vector<std::unique_ptr<ExprAST>> Args)
+        : Callee(Callee), Args(std::move(Args)) {}
+private:
+    std::string Callee;
+    std::vector<std::unique_ptr<ExprAST>> Args;
+};
+
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the function takes).
+class PrototypeAST {
+public:
+    PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+        : Name(Name), Args(std::move(Args)) {}
+private:
+    std::string Name;
+    std::vector<std::string> Args;
+};
+
+/// FunctionAST - This class represents a function definition itself.
+class FunctionAST {
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+            std::unique_ptr<ExprAST> Body)
+        : Proto(std::move(Proto)), Body(std::move(Body)) {}
+private:
+    std::unique_ptr<PrototypeAST> Proto;
+    std::unique_ptr<ExprAST> Body;
+};
+
+/// CurTok/getNextToken - Provice a simple token buffer. CurTok is the current
+/// token the parser is looking at. getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
+static int CurTok;
+static int getNextToken() {
+    return CurTok = gettok();
+}
+
+/// Error* - These are litter helper functions for error handling.
+std::unique_ptr<ExprAST> Error(const char *Str) {
+    fprintf(stderr, "Error: %s\n", Str);
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> ErrorP(const char *Str) {
+    Error(Str);
+    return nullptr;
+}
+
+/// numberexpr ::= number
+static std::unique_ptr<ExprAST> ParseNumberExpr() {
+    auto Result = llvm::make_unique<NumberExprAST>(NumVal);
+    getNextToken(); // consume the number
+    return std::move(Result);
+}
+
+/// parenexpr ::= '(' expression ')'
+static std::unique_ptr<ExprAST> ParseParenExpr() {
+    getNextToken(); // eat (.
+    auto V = ParseExpression();
+    if (!V) {
+        return nullptr;
+    }
+
+    if (CurTok != ')') {
+        return Error("expected ')'");
+    }
+    getNextToken(); // eat ).
+    return V;
+}
+
+/// identifierexpr
+///     ::= identifier                          variable reference
+///     ::= identifier '(' expression* ')'      function call
+static std::unique_ptr<ExprAST> ParseIdentifierExpr() {
+    std::string IdName = IdentifierStr;
+
+    getNextToken(); // eat identifier.
+
+    if (CurTok != '(') { // Simple variable ref.
+        return llvm::make_unique<VariableExprAST>(IdName);
+    }
+
+    // Call.
+    getNextToken(); // eat (
+    std::vector<std::unique_ptr<ExprAST>> Args;
+    if (CurTok != ')') {
+        while (1) {
+            if (auto Arg = ParseExpression()) {
+                Args.push_back(std::move(Arg));
+            }
+            else {
+                return nullptr;
+            }
+
+            if (CurTok == ')') {
+                break;
+            }
+
+            if (CurTok != ',') {
+                return Error("Expected ')' or ',' in argument list");
+            }
+
+            getNextToken();
+        }
+    }
+
+    // Eat the ')'.
+    getNextToken();
+
+    return llvm::make_unique<CallExprAST>(IdName, std::move(Args));
+}
+
+/// primary
+///     ::= identifierexpr
+///     ::= numberexpr
+///     ::= parenexpr
+static std::unique_ptr<ExprAST> ParsePrimary() {
+    switch(CurTok) {
+    case tok_identifier:
+        return ParseIdentifierExpr();
+    case tok_number:
+        return ParseNumberExpr();
+    case '(':
+        return ParseParenExpr();
+    default:
+        return Error("unknown token when expecting an expression");
+    }
+}
+
+int main(int argc, char const* argv[])
+{
+    return 0;
+}
