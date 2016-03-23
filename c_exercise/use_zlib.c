77eabdbd5cc0c0ff7a3cdd45b911bd8bbb82f964
dennis <mrvon@qq.com>
2016-03-23 17:38:20 +0800

    use_zlib

---
+++ c_exercise/use_zlib.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <zlib.h>
+
+// gcc -std=gnu99 use_zlib.c -lz
+
+void buff_print(const char* buff, unsigned long len) {
+	static char hex[] = "0123456789abcdef";
+
+    printf("%ld\n", len);
+    for (int i = 0; i < len; ++i) {
+        char byte = buff[i];
+
+        printf("%c", hex[(byte & 0xF0) >> 4]);
+        printf("%c", hex[(byte & 0x0F) >> 0]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char const* argv[]) {
+
+    char source[] = "fuck you every day";
+
+    unsigned long source_len = strlen(source) + 1;
+    unsigned long dest_len = 0;
+
+    buff_print(source, source_len);
+
+    dest_len = compressBound(source_len);
+
+    char* dest = (char*)malloc(dest_len);
+    assert(dest);
+
+    int status = compress(dest, &dest_len, source, source_len);
+
+    if (status != Z_OK) {
+        printf("compress error (%d)!\n", status);
+        return 0;
+    }
+
+    buff_print(dest, dest_len);
+
+    return 0;
+}
