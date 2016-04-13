de56e2cb29dddad53f4b50e66479cabb255e3322
dennis <mrvon@qq.com>
2016-04-13 23:14:37 +0800

    update

--- c_exercise/skip_list.c
+++ c_exercise/skip_list.c
@@ -82,4 +82,191 @@ randomLevel()
 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+#define PROBABILITY     0.5
+#define MAX_LEVEL       16
+
+struct list_node {
+    int key;
+    int val;
+    struct list_node* forward[1];
+};
+
+struct skip_list {
+    int level;
+    struct list_node* header;
+};
+
+static void
+random_init() {
+    srand(time(NULL));
+}
+
+static double
+random_range() {
+    // [0, 1]
+    return (double)rand() / (double)RAND_MAX ;
+}
+
+static int
+random_level() {
+    int level = 1;
+
+    while ((random_range() <= PROBABILITY) && (level < MAX_LEVEL)) {
+        level++;
+    }
+
+    return level;
+}
+
+static struct list_node*
+new_node(int level) {
+    assert(level >= 1);
+    struct list_node* node = (struct list_node*)malloc(
+            sizeof(*node) + sizeof(struct list_node*) * (level - 1)
+            );
+    assert(node);
+    return node;
+}
+
+static struct skip_list*
+new_list() {
+    struct skip_list* list = (struct skip_list*)malloc(sizeof(*list));
+    assert(list);
+
+    list->level = 1;
+    list->header = new_node(MAX_LEVEL);
+
+    return list;
+}
+
+static void
+del_list(struct skip_list* list) {
+    assert(list);
+    free((void*)list);
+}
+
+static int
+search(struct skip_list* list, int search_key) {
+    struct list_node* x = list->header;
+
+    int i;
+    for (i = list->level - 1; i >= 0; --i) {
+        while (x->forward[i] && x->forward[i]->key < search_key) {
+            x = x->forward[i];
+        }
+    }
+
+    x = x->forward[0];
+
+    if (x && x->key == search_key) {
+        return x->val;
+    } else {
+        return 0;
+    }
+}
+
+static void
+insert(struct skip_list* list, int insert_key, int insert_val) {
+    struct list_node** update = (struct list_node**)malloc(sizeof(struct list_node*) * MAX_LEVEL);
+    struct list_node* x = list->header;
+    int level;
+    int i;
+
+    assert(update);
+
+    for (i = list->level - 1; i >= 0; --i) {
+        while (x->forward[i] && x->forward[i]->key < insert_key) {
+            x = x->forward[i];
+        }
+        update[i] = x;
+    }
+
+    x = x->forward[0];
+
+    if (x && x->key == insert_key) {
+        x->val = insert_val;
+    } else {
+        level = random_level();
+
+        if (level > list->level) {
+            for (i = list->level; i <= level - 1; ++i) {
+                update[i] = list->header;
+            }
+            list->level = level;
+        }
+
+        x = new_node(level);
+        x->key = insert_key;
+        x->val = insert_val;
+
+        for (i = 0; i <= level - 1; ++i) {
+            x->forward[i] = update[i]->forward[i];
+            update[i]->forward[i] = x;
+        }
+    }
+
+    free(update);
+}
+
+static void
+delete(struct skip_list* list, int delete_key) {
+    struct list_node** update = (struct list_node**)malloc(sizeof(struct list_node*) * MAX_LEVEL);
+    struct list_node* x = list->header;
+    int i;
+
+    assert(update);
+
+    for (i = list->level - 1; i >= 0; --i) {
+        while (x->forward && x->forward[i]->key < delete_key) {
+            x = x->forward[i];
+        }
+        update[i] = x;
+    }
+
+    x = x->forward[0];
+
+    if (x && x->key == delete_key) {
+        for (i = 0; i <= list->level - 1; ++i) {
+            assert(update[i]);
+            if (update[i]->forward[i] != x) {
+                break;
+            }
+            update[i]->forward[i] = x->forward[i];
+        }
+
+        free(x);
+
+        while (list->level > 1 && list->header->forward[list->level - 1] == NULL) {
+            --list->level;
+        }
+    }
+
+    free(update);
+}
+
+
+int main() {
+    random_init();
+
+    struct skip_list* list = new_list();
+
+    insert(list, 1023, 20481);
+    insert(list, 1024, 20482);
+    insert(list, 1025, 20483);
+    insert(list, 1026, 20484);
+
+    printf("key = %d val = %d\n", 1023, search(list, 1023));
+    printf("key = %d val = %d\n", 1024, search(list, 1024));
+    printf("key = %d val = %d\n", 1025, search(list, 1025));
+    printf("key = %d val = %d\n", 1026, search(list, 1026));
+
+    del_list(list);
+
+    return 0;
+}
 
