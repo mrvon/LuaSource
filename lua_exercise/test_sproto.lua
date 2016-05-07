102061c66cae8cdc3d1b46500291a3aeae4615bf
Dennis <mrvon@qq.com>
2016-05-07 19:15:27 +0800

    update

--- lua_exercise/test_sproto.lua
+++ lua_exercise/test_sproto.lua
@@ -5,76 +5,6 @@ local crypt = require "crypt"
 local parser = require "sprotoparser"
 local sproto = require "sproto.core"
 
-function cclog(obj)  
-    local getIndent, quoteStr, wrapKey, wrapVal, isArray, dumpObj  
-    getIndent = function(level)  
-        return string.rep("\t", level)  
-    end  
-    quoteStr = function(str)  
-        str = string.gsub(str, "[%c\\\"]", {  
-            ["\t"] = "\\t",  
-            ["\r"] = "\\r",  
-            ["\n"] = "\\n",  
-            ["\""] = "\\\"",  
-            ["\\"] = "\\\\",  
-        })  
-        return '"' .. str .. '"'  
-    end  
-    wrapKey = function(val)  
-        if type(val) == "number" then  
-            return "[" .. val .. "]"  
-        elseif type(val) == "string" then  
-            return "[" .. quoteStr(val) .. "]"  
-        else  
-            return "[" .. tostring(val) .. "]"  
-        end  
-    end  
-    wrapVal = function(val, level)  
-        if type(val) == "table" then  
-            return dumpObj(val, level)  
-        elseif type(val) == "number" then  
-            return val  
-        elseif type(val) == "string" then  
-            return quoteStr(val)  
-        else  
-            return tostring(val)  
-        end  
-    end  
-    local isArray = function(arr)  
-        local count = 0   
-        for k, v in pairs(arr) do  
-            count = count + 1   
-        end   
-        for i = 1, count do  
-            if arr[i] == nil then  
-                return false  
-            end   
-        end   
-        return true, count  
-    end  
-    dumpObj = function(obj, level)  
-        if type(obj) ~= "table" then  
-            return wrapVal(obj)  
-        end  
-        level = level + 1  
-        local tokens = {}  
-        tokens[#tokens + 1] = "{"  
-        local ret, count = isArray(obj)  
-        if ret then  
-            for i = 1, count do  
-                tokens[#tokens + 1] = getIndent(level) .. wrapVal(obj[i], level) .. ","  
-            end  
-        else  
-            for k, v in pairs(obj) do  
-                tokens[#tokens + 1] = getIndent(level) .. wrapKey(k) .. " = " .. wrapVal(v, level) .. ","  
-            end  
-        end  
-        tokens[#tokens + 1] = getIndent(level - 1) .. "}"  
-        return table.concat(tokens, "\n")  
-    end  
-    return dumpObj(obj, 0)  
-end
-
 local schema = [[
 .Person {
     name 0 : string
@@ -128,6 +58,6 @@ local unpack_stream = assert(sproto.unpack(pack_stream))
 
 local origin_packet = sproto.decode(type_object, unpack_stream)
 
-print(cclog(origin_packet))
+-- print(inspire(origin_packet))
 
 -- RPC
