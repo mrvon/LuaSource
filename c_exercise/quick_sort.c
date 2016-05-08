4ca1b101ca5f73f1debd5e45793af1a47962d093
Dennis <mrvon@qq.com>
2016-05-08 15:07:43 +0800

    update

---
+++ c_exercise/quick_sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * Quick sort with recursion
+ */
+
+/* Lomuto partition scheme */
+void lomuto_qsort() {
+}
+
+/* Hoare partition scheme */
+/* Random partition scheme */
+
+/* Quick sort with stack */
+
+int main() {
+    return 0;
+}
