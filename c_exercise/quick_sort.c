f9838e2037217572b0646d3499c1614e8c7e2e5e
Mrvon <mrvon@qq.com>
2016-05-08 19:50:56 +0800

    test performance

--- c_exercise/quick_sort.c
+++ c_exercise/quick_sort.c
@@ -85,6 +85,31 @@ hoare_qsort(int arr[], int left, int right) {
     }
 }
 
+/* median of 3 partition scheme
+ * */
+static int
+median_of_three_partition(int arr[], int left, int right) {
+    int mid = left + (right - left) / 2;
+
+    if (arr[left] >= arr[mid] && arr[left] <= arr[right]) {
+        swap(arr, left, right);
+    } else if (arr[mid] >= arr[left] && arr[mid] <= arr[right]) {
+        swap(arr, mid, right);
+    }
+
+    return lomuto_partition(arr, left, right);
+}
+
+static void
+median_of_three_qsort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = median_of_three_partition(arr, left, right);
+        median_of_three_qsort(arr, left, mid-1);
+        median_of_three_qsort(arr, mid+1, right);
+    }
+}
+
+
 static int
 rand_num(int left, int right) {
     assert(left <= right);
@@ -109,22 +134,59 @@ randomized_sort(int arr[], int left, int right) {
     }
 }
 
+int*
+gen_test_arr(int size) {
+    int* arr = (int*)malloc(size * sizeof(int));
+    assert(arr);
+
+    int i;
+    for (i = 0; i < size; ++i) {
+        arr[i] = rand_num(0, size);
+    }
+
+    return arr;
+}
+
+void
+del_test_arr(int* arr) {
+    assert(arr);
+    free((void*)arr);
+}
+
 int
 main() {
     srand(time(NULL));
 
-    int table[] = {
-        13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21,
-    };
+    int size = 100000;
 
     int left = 0;
-    int right = sizeof(table) / sizeof(int) - 1;
+    int right = size - 1;
 
-    lomuto_qsort(table, left, right);
-    hoare_qsort(table, left, right);
-    randomized_sort(table, left, right);
+    int* arr = gen_test_arr(size);
 
-    check(table, left, right);
+    /* that's quick when element in array is random shuffle */
+    lomuto_qsort(arr, left, right);
+    printf("1 lomuto_qsort is done\n");
+
+    /* that's very slow when all element in array is sorted */
+    lomuto_qsort(arr, left, right);
+    printf("2 lomuto_qsort is done\n");
+
+    /* that's very slow when all element in array is sorted */
+    hoare_qsort(arr, left, right);
+    printf("3 hoare_qsort is done\n");
+
+    /* performance is always pertty good */
+    randomized_sort(arr, left, right);
+    printf("4 randomized_sort is done\n");
+
+    /* performance is always pertty good */
+    median_of_three_qsort(arr, left, right);
+    printf("5 median_of_three_qsort is done\n");
+
+    check(arr, left, right);
+
+    del_test_arr(arr);
 
     return 0;
 }
