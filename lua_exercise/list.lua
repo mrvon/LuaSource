76ddc9bddd5b476ac50eebe4971c19a036fa9763
Dennis <mrvon@qq.com>
2016-05-21 14:04:56 +0800

    update list

--- lua_exercise/list.lua
+++ lua_exercise/list.lua
@@ -4,17 +4,6 @@ function List.new()
     return {first = 0, last = -1}
 end
 
-function List.Debug(list)
-    print(string.format("FI:%d LI:%d", list.first, list.last))
-end
-
-function List.FixIndex(list)
-    if list.first > list.last then
-        list.first = 0
-        list.last = -1
-    end
-end
-
 function List.pushfirst(list, value)
     local first = list.first - 1
     list.first = first
@@ -36,7 +25,7 @@ function List.popfirst(list)
     list[first] = nil   -- to allow garbage collection
     list.first = first + 1
 
-    List.FixIndex(list)
+    List.__fix_index(list)
     return value
 end
 
@@ -49,27 +38,57 @@ function List.poplast(list)
     list[last] = nil    -- to allow garbage collection
     list.last = last - 1
 
-    List.FixIndex(list)
+    List.__fix_index(list)
     return value
 end
 
-function List.traverse(list)
-    local first = list.first
-    local last = list.last
-    while first <= last do
-        print(list[first])
-        first = first + 1
+function List.getfirst(list)
+    return list[list.first]
+end
+
+function List.getlast(list)
+    return list[list.last]
+end
+
+function List.isempty(list)
+    if list.first > list.last then
+        return true
+    else
+        return false
+    end
+end
+
+function List.__fix_index(list)
+    if list.first > list.last then
+        list.first = 0
+        list.last = -1
+    end
+end
+
+--------------------------------------------------------------------------------
+
+local function debug(list)
+    print(string.format("FI:%d LI:%d", list.first, list.last))
+end
+
+
+local function traverse(list)
+    while true do
+        local val = List.getfirst(list)
+        if val == nil then
+            break
+        end
+        List.popfirst(list)
+
+        print(val)
     end
 end
 
 l = List.new()
+
 List.pushfirst(l, "Hello ")
 List.pushfirst(l, "Debug ")
 List.pushlast(l, "World")
 List.pushlast(l, "!")
-List.traverse(l)
-List.popfirst(l)
-List.poplast(l)
-List.poplast(l)
-List.poplast(l)
-List.Debug(l)
+
+traverse(l)
