b72ac7745441b11e5990ff98543479debdd99377
Dennis <mrvon@qq.com>
2016-05-21 14:10:56 +0800

    oo style list

--- lua_exercise/list.lua
+++ lua_exercise/list.lua
@@ -1,67 +1,65 @@
 List = {}
 
 function List.new()
-    return {first = 0, last = -1}
+    local mt = {
+        __index = List
+    }
+    return setmetatable({
+        first = 0,
+        last = -1,
+    }, mt)
 end
 
-function List.pushfirst(list, value)
-    local first = list.first - 1
-    list.first = first
-    list[first] = value
+function List:pushfirst(value)
+    local first = self.first - 1
+    self.first = first
+    self[first] = value
 end
 
-function List.pushlast(list, value)
-    local last = list.last + 1
-    list.last = last
-    list[last] = value
+function List:pushlast(value)
+    local last = self.last + 1
+    self.last = last
+    self[last] = value
 end
 
-function List.popfirst(list)
-    local first = list.first
-    if first > list.last then
+function List:popfirst()
+    local first = self.first
+    if first > self.last then
         error("list is empty")
     end
-    local value = list[first]
-    list[first] = nil   -- to allow garbage collection
-    list.first = first + 1
+    local value = self[first]
+    self[first] = nil   -- to allow garbage collection
+    self.first = first + 1
 
-    List.__fix_index(list)
+    self:__fix_index()
     return value
 end
 
-function List.poplast(list)
-    local last = list.last
-    if list.first > last then
+function List:poplast()
+    local last = self.last
+    if self.first > last then
         error("list is empty")
     end
-    local value = list[last]
-    list[last] = nil    -- to allow garbage collection
-    list.last = last - 1
+    local value = self[last]
+    self[last] = nil    -- to allow garbage collection
+    self.last = last - 1
 
-    List.__fix_index(list)
+    self:__fix_index()
     return value
 end
 
-function List.getfirst(list)
-    return list[list.first]
+function List:getfirst()
+    return self[self.first]
 end
 
-function List.getlast(list)
-    return list[list.last]
+function List:getlast()
+    return self[self.last]
 end
 
-function List.isempty(list)
-    if list.first > list.last then
-        return true
-    else
-        return false
-    end
-end
-
-function List.__fix_index(list)
-    if list.first > list.last then
-        list.first = 0
-        list.last = -1
+function List:__fix_index()
+    if self.first > self.last then
+        self.first = 0
+        self.last = -1
     end
 end
 
@@ -74,11 +72,11 @@ end
 
 local function traverse(list)
     while true do
-        local val = List.getfirst(list)
+        local val = list:getfirst()
         if val == nil then
             break
         end
-        List.popfirst(list)
+        list:popfirst()
 
         print(val)
     end
@@ -86,9 +84,9 @@ end
 
 l = List.new()
 
-List.pushfirst(l, "Hello ")
-List.pushfirst(l, "Debug ")
-List.pushlast(l, "World")
-List.pushlast(l, "!")
+l:pushfirst("Hello")
+l:pushfirst("Debug")
+l:pushlast("World")
+l:pushlast("!")
 
 traverse(l)
