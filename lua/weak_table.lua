f90c0f898e1343825a97d674c71ccac274f4fa75
Dennis <mrvon@qq.com>
2016-06-03 16:49:22 +0800

    update

--- lua/weak_table.lua
+++ lua/weak_table.lua
@@ -1,27 +1,102 @@
-a = {}
-b = {
-    __mode = "k",
-}
-setmetatable(a, b)
+-- if either the key or the value is collected, the whole pair is removed from the table.
 
-key = {}
-a[key] = 1
 
-key = {}
-a[key] = 2
+local function mode_k()
+    local t = {}
+    local mt = {
+        __mode = "k",
+    }
+    -- weak key, strong value
+    setmetatable(t, mt)
 
-key_2 = {}
-a[1] = key_2
+    local weak_key_1 = {}
+    local weak_key_2 = {}
+    local strong_val_1 = {}
+    local strong_val_2 = {}
 
-key_2 = {}
-a[2] = key_2
+    t[weak_key_1] = strong_val_1
+    t[weak_key_2] = strong_val_2
 
--- force a garbage collection cycle
-collectgarbage()
+    -- value 1, value 2 will not be collected, it's strong value reference in
+    -- table
+    strong_val_1 = nil
+    strong_val_2 = nil
 
-for k, v in pairs(a) do
-    print(k, v)
+    -- force a garbage collection cycle
+    collectgarbage()
+
+    for k, v in pairs(t) do
+        print(k, v)
+    end
+
+    print("------------------------------------------------")
+
+    -- key 1 will be collected, the whole pair is remove
+    weak_key_1 = nil
+
+    -- force a garbage collection cycle
+    collectgarbage()
+
+    for k, v in pairs(t) do
+        print(k, v)
+    end
 end
+
+local function mode_v()
+    local t = {}
+    local mt = {
+        __mode = "v",
+    }
+    -- strong key, weak value
+    setmetatable(t, mt)
+
+    local strong_key_1 = {}
+    local string_key_2 = {}
+    local weak_val_1 = {}
+    local weak_val_2 = {}
+
+    t[strong_key_1] = weak_val_1
+    t[string_key_2] = weak_val_2
+
+    -- value 1 will be collected, the whole pair is remove
+    weak_val_1 = nil
+
+    -- force a garbage collection cycle
+    collectgarbage()
+
+    for k, v in pairs(t) do
+        print(k, v)
+    end
+end
+
+local function mode_kv()
+    local t = {}
+    local mt = {
+        __mode = "kv",
+    }
+    -- weak key, weak value
+    setmetatable(t, mt)
+
+    -- will not be collected
+    t[1] = 1
+    t[2] = "World"
+    t["Hello"] = 1
+
+    -- will be collected
+    t[3] = {}
+    t[{}] = 3
+
+    -- force a garbage collection cycle
+    collectgarbage()
+
+    for k, v in pairs(t) do
+        print(k, v)
+    end
+end
+
+-- mode_k()
+-- mode_v()
+-- mode_kv()
 
 --------------------------------------------------------------------------------
 
