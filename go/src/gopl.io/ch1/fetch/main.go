bbabfabbac789513714c7a07615ad28edf19e7b3
Dennis <mrvon@qq.com>
2016-06-28 17:12:41 +0800

    update

---
+++ go/src/gopl.io/ch1/fetch/main.go
@@ -0,0 +1,57 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func original_main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", b)
+	}
+}
+
+func main() {
+	http_prefix := "http://"
+
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, http_prefix) {
+			url = http_prefix + url
+		}
+
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+	}
+}
