3dfc0230e86e0e46a3f91b9229f8edf054619e98
Dennis <mrvon@qq.com>
2016-07-11 01:37:38 +0800

    update

---
+++ go/src/gopl.io/ch4/rev/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+// reverse reverses a slice of ints in place.
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	b := [...]int{0, 1, 2, 3, 4, 5}
+
+	// array is comparable
+	fmt.Println(a == b)
+
+	fmt.Println(a)
+	reverse(a[:])
+	fmt.Println(a)
+
+	s1 := []int{0, 1, 2, 3, 4, 5}
+	// Rotate s left by two positions.
+	reverse(s1[:2])
+	reverse(s1[2:])
+	reverse(s1)
+	fmt.Println(s1)
+
+	// Rotate s right by two positions.
+	s2 := []int{0, 1, 2, 3, 4, 5}
+	reverse(s2)
+	reverse(s2[:2])
+	reverse(s2[2:])
+	fmt.Println(s2)
+
+	// slice is not comparable (slice can only compare to nil)
+	// fmt.Println(s1 == s2)
+
+	fmt.Printf("%T %T %T\n", a, s1, s2)
+}
