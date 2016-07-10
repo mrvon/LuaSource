b2104ee080578d0d21d87eceae21f04a08ce5493
Dennis <mrvon@qq.com>
2016-07-11 05:00:01 +0800

    update

--- go/src/gopl.io/ch4/rev/main.go
+++ go/src/gopl.io/ch4/rev/main.go
@@ -11,6 +11,12 @@ func reverse(s []int) {
 	}
 }
 
+func reverse_using_arr_ptr(p *[6]int) {
+	for i, j := 0, len(*p)-1; i < j; i, j = i+1, j-1 {
+		(*p)[i], (*p)[j] = (*p)[j], (*p)[i]
+	}
+}
+
 func main() {
 	a := [...]int{0, 1, 2, 3, 4, 5}
 	b := [...]int{0, 1, 2, 3, 4, 5}
@@ -40,4 +46,10 @@ func main() {
 	// fmt.Println(s1 == s2)
 
 	fmt.Printf("%T %T %T\n", a, s1, s2)
+
+	c := [...]int{0, 1, 2, 3, 4, 5}
+
+	fmt.Println(c)
+	reverse_using_arr_ptr(&c)
+	fmt.Println(c)
 }
