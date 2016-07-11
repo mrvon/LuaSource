3c3340c4f5866fecd7b4900fdab71236e96e51a4
Dennis <mrvon@qq.com>
2016-07-12 02:47:38 +0800

    update

---
+++ go/src/gopl.io/ch4/moive/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Moive struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
+}
+
+var moives = []Moive{
+	{Title: "Casablanca", Year: 1942, Color: false,
+		Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+	{Title: "Cool Hand Luke", Year: 1967, Color: true,
+		Actors: []string{"Paul Newman"}},
+	{Title: "Bullitt", Year: 1968, Color: true,
+		Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+}
+
+func main() {
+	{
+		data, err := json.Marshal(moives)
+		if err != nil {
+			log.Fatalf("JSON marshaling failed: %s", err)
+		}
+
+		fmt.Printf("%s\n", data)
+	}
+
+	{
+		data, err := json.MarshalIndent(moives, "", "    ")
+		if err != nil {
+			log.Fatalf("JSON marshaling failed: %s", err)
+		}
+
+		fmt.Printf("%s\n", data)
+	}
+}
