8c0ca576454199654070ebd182aa721982d5fc02
Dennis <mrvon@qq.com>
2016-08-23 17:11:36 +0800

    temp

--- go/src/gopl.io/ch0/dirmd5/dirmd5.go
+++ go/src/gopl.io/ch0/dirmd5/dirmd5.go
@@ -3,13 +3,13 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
+	"image/png"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
-
-var output_map = make(map[string]string)
 
 func filterDir(dir string) bool {
 	if dir == ".git" || dir == ".svn" {
@@ -19,32 +19,22 @@ func filterDir(dir string) bool {
 	}
 }
 
-func walkDir(dir string) {
+func filterType(file string) bool {
+	return true
+}
+
+func walkDir(dir string, callback func(filename string)) {
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			if filterDir(entry.Name()) {
 				continue
 			}
 			subDir := filepath.Join(dir, entry.Name())
-			walkDir(subDir)
+			walkDir(subDir, callback)
 		} else {
 			filename := filepath.Join(dir, entry.Name())
 
-			f, err := os.Open(filename)
-			if err != nil {
-				continue
-			}
-
-			data, err := ioutil.ReadAll(f)
-			if err != nil {
-				continue
-			}
-
-			file_str := fmt.Sprintf("%q", filename)
-			md5_str := fmt.Sprintf("\"%x\"", md5.Sum(data))
-			output_map[file_str] = md5_str
-
-			f.Close()
+			callback(filename)
 		}
 	}
 }
@@ -59,9 +49,38 @@ func dirents(dir string) []os.FileInfo {
 
 func main() {
 	root_dir := "/home/dennis/skynet"
-	md5_file := "/home/dennis/verfication.lua"
+	md5_file := "/home/dennis/md5.lua"
+	png_file := "/home/dennis/map.bin"
 
-	walkDir(root_dir)
+	var md5_map = make(map[string]string)
+	var hw_list = make([]string, 0)
+
+	walkDir(root_dir, func(filename string) {
+		f, err := os.Open(filename)
+		if err != nil {
+			return
+		}
+
+		data, err := ioutil.ReadAll(f)
+		if err != nil {
+			return
+		}
+
+		file_str := fmt.Sprintf("%q", filename)
+
+		md5_str := fmt.Sprintf("\"%x\"", md5.Sum(data))
+		md5_map[file_str] = md5_str
+
+		reader := strings.NewReader(string(data))
+		conf, err := png.DecodeConfig(reader)
+		if err != nil {
+			return
+		}
+
+		hw_list = append(hw_list, fmt.Sprintf("%s %d %d", file_str, conf.Height, conf.Width))
+
+		f.Close()
+	})
 
 	f, err := os.Create(md5_file)
 	if err != nil {
@@ -71,7 +90,7 @@ func main() {
 
 	fmt.Fprint(f, "return {\n")
 
-	for k, v := range output_map {
+	for k, v := range md5_map {
 		fmt.Fprint(f, "\t[")
 		fmt.Fprint(f, k)
 		fmt.Fprint(f, "]")
@@ -81,4 +100,18 @@ func main() {
 	}
 
 	fmt.Fprint(f, "}\n")
+
+	f.Close()
+
+	f, err = os.Create(png_file)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for i := range hw_list {
+		fmt.Fprintln(f, i)
+	}
+
+	f.Close()
 }
