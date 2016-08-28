d142ddf19c539a1a225ff467dbbf37a35feefcb7
dennis <mrvon@qq.com>
2016-08-28 23:03:05 +0800

    update

---
+++ c/unsigned_char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char c = -1;
+
+    // in mac, type char is signed.
+
+    printf("%d\n", c);
+
+    // if you want to store c in a unsigned int, keep its original bit
+    
+    // you should convert it to unsigned char first
+    unsigned int ui = (unsigned char)c;
+
+    printf("%u\n", ui);
+
+    // otherwise, the result is different.
+    unsigned int i = c;
+
+    printf("%u\n", i);
+}
