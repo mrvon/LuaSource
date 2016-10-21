b34aa89ad412d48e12dc56fd3bf591002108ca6a
dennis <mrvon@qq.com>
2016-10-21 19:56:21 +0800

    update

--- go/src/leetcode/104_maximum_depth_of_binary_tree.go
+++ go/src/leetcode/104_maximum_depth_of_binary_tree.go
@@ -1,3 +1,5 @@
+package main
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -7,15 +9,15 @@
  * }
  */
 func maxDepth(root *TreeNode) int {
-    if root != nil {
-        x := maxDepth(root.Left)
-        y := maxDepth(root.Right)
-        if x > y {
-            return x + 1
-        } else {
-            return y + 1
-        }
-    } else {
-        return 0
-    }
+	if root != nil {
+		x := maxDepth(root.Left)
+		y := maxDepth(root.Right)
+		if x > y {
+			return x + 1
+		} else {
+			return y + 1
+		}
+	} else {
+		return 0
+	}
 }
