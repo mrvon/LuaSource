e03345a4ab3c32f756d992f42f9ff93cc086c2b8
Dennis <mrvon@qq.com>
2016-11-01 08:57:14 +0800

    update

--- l/random_shuffle.lua
+++ l/random_shuffle.lua
@@ -1,3 +1,11 @@
+-- Fisher–Yates_shuffle
+--
+-- To shuffle an array a of n elements (indices 0..n-1):
+--
+-- for i from n−1 downto 1 do
+--      j ← random integer such that 0 ≤ j ≤ i
+--      exchange a[j] and a[i]
+
 math.randomseed(os.time())
 
 function random_shuffle(list)
