88da2d0ea259a3b73e8cc869e716cbd46ea9b7e5
Dennis <mrvon@qq.com>
2016-11-01 11:42:36 +0800

    update

---
+++ go/src/leetcode/357_count_numbers_with_unique_digits.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func countNumbersWithUniqueDigits(n int) int {
+	return n
+}
+
+func main() {
+	fmt.Println(countNumbersWithUniqueDigits(0))
+	fmt.Println(countNumbersWithUniqueDigits(1))
+	fmt.Println(countNumbersWithUniqueDigits(2))
+	fmt.Println(countNumbersWithUniqueDigits(3))
+}
