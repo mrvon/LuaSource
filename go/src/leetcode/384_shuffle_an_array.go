d8a3198341e471f5033d43fd04415f5324171ab2
Dennis <mrvon@qq.com>
2016-11-01 08:57:04 +0800

    update

--- go/src/leetcode/384_shuffle_an_array.go
+++ go/src/leetcode/384_shuffle_an_array.go
@@ -1,21 +1,25 @@
+// Fisher–Yates_shuffle
+//
+// To shuffle an array a of n elements (indices 0..n-1):
+//
+// for i from n−1 downto 1 do
+//      j ← random integer such that 0 ≤ j ≤ i
+//      exchange a[j] and a[i]
+
 package main
 
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 type Solution struct {
 	base []int
 	buff []int
-	rand *rand.Rand
 }
 
 func Constructor(nums []int) Solution {
 	var s Solution
-
-	s.rand = rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 
 	for i := 0; i < len(nums); i++ {
 		s.base = append(s.base, nums[i])
@@ -27,7 +31,10 @@ func Constructor(nums []int) Solution {
 
 /** Resets the array to its original configuration and return it. */
 func (this *Solution) Reset() []int {
-	return this.base
+	for i := 0; i < len(this.base); i++ {
+		this.buff[i] = this.base[i]
+	}
+	return this.buff
 }
 
 /** Returns a random shuffling of the array. */
@@ -38,14 +45,14 @@ func (this *Solution) Shuffle() []int {
 	}
 
 	i := s - 1
-	r := this.rand.Int() % s
 
-	for r >= 0 {
+	for i > 0 {
+		r := rand.Int() % (i + 1)
 		// swap
 		temp := this.buff[i]
 		this.buff[i] = this.buff[r]
 		this.buff[r] = temp
-		r--
+		i--
 	}
 
 	return this.buff
@@ -59,8 +66,9 @@ func (this *Solution) Shuffle() []int {
  */
 
 func main() {
-	obj := Constructor([]int{1, 2, 3, 4, 5, 6})
-	fmt.Println(obj.Shuffle())
-	fmt.Println(obj.Reset())
-	fmt.Println(obj.Shuffle())
+	obj := Constructor([]int{1, 2, 3})
+	for i := 1; i < 20; i++ {
+		fmt.Println(obj.Reset())
+		fmt.Println(obj.Shuffle())
+	}
 }
