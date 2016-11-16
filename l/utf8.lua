1e136d5b00ec86f681f943429db91aadc1854127
dennis <mrvon@qq.com>
2016-11-16 15:27:57 +0800

    update

--- l/utf8.lua
+++ l/utf8.lua
@@ -1,22 +1,32 @@
 -- utf8
 
 local utf8_char = "€"
-local utf8_char_list = "€€€€"
+local utf8_char_sequence = "€€€€"
 
+-- this *code point* is decimal representation
 local code_point = utf8.codepoint(utf8_char)
-print(code_point)
+
+-- print *code point* with hexidecimal representation
+print(string.format("Code Point: %X", code_point))
+
+-- print utf8 char corresponds to *code point*
 print(utf8.char(code_point))
 
 -- utf8.charpattern
 
-for p, c in utf8.codes(utf8_char_list) do
-    print("Position:", p, "CodePoint:", c)
+-- traversal utf8 char sequence
+for p, c in utf8.codes(utf8_char_sequence) do
+    print(string.format("Position(in the sequence): %d\tCodePoint: %X", p, c))
 end
 
-print("Len of char list", utf8.len(utf8_char_list))
+-- how many utf8 char in this sequence
+print("Len of char sequence", utf8.len(utf8_char_sequence))
 
-io.write("Utf8 Encoding Byte List:", "\t")
+io.write("utf8 MEMORY Byte List: ")
 for i = 1, string.len(utf8_char) do
-    io.write(string.byte(utf8_char, i), "\t")
+    io.write(string.format("%X ", string.byte(utf8_char, i)))
 end
 io.write("\n")
+
+-- use utf8 in literal string
+print("\u{20AC}")
