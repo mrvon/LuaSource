acfae086236f9dbad48033e117583429ba7a6c41
dennis <mrvon@qq.com>
2016-11-22 12:06:10 +0800

    update

---
+++ go/src/leetcode/406_queue_reconstruction_by_height.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type List [][]int
+
+func (v List) Len() int {
+	return len(v)
+}
+
+func (v List) Less(i, j int) bool {
+	if v[i][0] > v[j][0] {
+		return true
+	} else if v[i][0] == v[j][0] {
+		if v[i][1] <= v[j][1] {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+
+func (v List) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
+
+func reconstructQueue(people [][]int) [][]int {
+	var l List = people
+
+	sort.Sort(l)
+
+	for i := 0; i < len(l); i++ {
+		j := l[i][1]
+		// INSERT into index j
+		temp := l[i]
+		for k := i - 1; k >= j; k-- {
+			l[k+1] = l[k]
+		}
+		l[j] = temp
+	}
+
+	return l
+}
+
+func main() {
+	fmt.Println(reconstructQueue([][]int{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}}))
+}
