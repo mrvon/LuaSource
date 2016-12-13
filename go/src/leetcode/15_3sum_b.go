c78b0234ac132808bb4023d8ee927a74b40db073
dennis <mrvon@qq.com>
2016-12-13 10:37:57 +0800

    accept

---
+++ go/src/leetcode/15_3sum_b.go
@@ -0,0 +1,58 @@
+// Key: write down and found the rule.
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func threeSum(nums []int) [][]int {
+	var result [][]int
+
+	n := len(nums)
+
+	sort.Ints(nums)
+
+	i := 0
+	for i < n {
+		j := i + 1
+		for j < n {
+			k := j + 1 // left range
+			l := n - 1 // right range
+			// binary search
+			target := -nums[i] - nums[j]
+			for k <= l {
+				m := k + (l-k)/2
+				if target == nums[m] {
+					result = append(result, []int{nums[i], nums[j], nums[m]})
+					break
+				} else if target < nums[m] {
+					l = m - 1
+				} else {
+					k = m + 1
+				}
+			}
+			// next j
+			j++
+			// skip same element
+			for j < n && nums[j-1] == nums[j] {
+				j++
+			}
+		}
+		// next i
+		i++
+		// skip same element
+		for i < n && nums[i-1] == nums[i] {
+			i++
+		}
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(threeSum([]int{}))
+	fmt.Println(threeSum([]int{0}))
+	fmt.Println(threeSum([]int{0, 0, 0}))
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
+}
