1cadf06615f0a7f971fca8bd406578ae311bfa49
dennis <mrvon@qq.com>
2016-12-14 17:56:54 +0800

    init 3

--- go/src/leetcode/regexp.go
+++ go/src/leetcode/regexp.go
@@ -13,6 +13,9 @@ func __re2post(re string, i int, postfix []byte) ([]byte, int) {
 				atom--
 				postfix = append(postfix, '$')
 			}
+			atom++
+
+			// sub regexp
 			postfix, i = __re2post(re, i+1, postfix)
 		} else if c == ')' {
 			if atom >= 2 {
@@ -51,13 +54,19 @@ func re2post(re string) string {
 	return string(postfix)
 }
 
+func assert(expect string, result string) {
+	if result != expect {
+		panic(fmt.Sprintf("Assert failed!, Expect %s, Get %s", expect, result))
+	}
+}
+
 func main() {
-	fmt.Println(re2post("abba"))     // ab$b$a$
-	fmt.Println(re2post("abba(ab)")) // ab$b$a$ab$$
-	fmt.Println(re2post("a(bb)+a"))  // abb$+$a$
-	fmt.Println(re2post("(abb)+a"))  // ab$b$+a$
-	fmt.Println(re2post("a(bb)+a"))  // abb$+$a$
-	// fmt.Println(re2post("a(bb|c)+a"))     // abb$c|+$a$
-	// fmt.Println(re2post("a(bb|c*)+a"))    // abb$c*|+$a$
-	// fmt.Println(re2post("a(bb(ccc)*)+a")) // abbcc$c$*$$+$a$
+	assert(re2post("abba"), "ab$b$a$")
+	assert(re2post("abba(ab)"), "ab$b$a$ab$$")
+	assert(re2post("a(bb)+a"), "abb$+$a$")
+	assert(re2post("(abb)+a"), "ab$b$+a$")
+	assert(re2post("a(bb)+a"), "abb$+$a$")
+	// assert(re2post("a(bb|c)+a"), "abb$c|+$a$")
+	// assert(re2post("a(bb|c*)+a"), "abb$c*|+$a$")
+	assert(re2post("a(bb(ccc)*)+a"), "abb$cc$c$*$+$a$")
 }
