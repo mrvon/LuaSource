9e5e67c19dcfd08e2b7ec5f22986be3c54838104
dennis <mrvon@qq.com>
2018-03-12 20:21:45 +0800

    update

--- c/network/getaddrinfo.c
+++ c/network/getaddrinfo.c
@@ -1,5 +1,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <errno.h>
 
@@ -7,30 +9,30 @@
 #include <string.h>
 
 int main() {
-    struct addrinfo hints;
-    struct addrinfo* result;
-    struct addrinfo* rp;
+    struct addrinfo ai_hints;
+    struct addrinfo* ai_list;
+    struct addrinfo* ai_ptr;
 
-    const char* host = "0.0.0.0";
-    const char* port = "5000";
+	memset(&ai_hints, 0, sizeof(ai_hints));
 
-    if (getaddrinfo(host, port, NULL, &result) != 0) {
+    const char* host = "www.163.com";
+
+    if (getaddrinfo(host, NULL, NULL, &ai_list) != 0) {
         fprintf(stderr, "%s\n", strerror(errno));
         return 0;
     }
 
-    for (rp = result; rp != NULL; rp = rp->ai_next) {
-        fprintf(stdout,
-                "-----------------------------------------\n"
-                "ai_flags(%d)\n"
-                "ai_family(%d)\n"
-                "ai_sockettype(%d)\n"
-                "ai_protocol(%d)\n",
-                rp->ai_flags,
-                rp->ai_family,
-                rp->ai_socktype,
-                rp->ai_protocol);
+    for (ai_ptr = ai_list; ai_ptr != NULL; ai_ptr = ai_ptr->ai_next) {
+        struct sockaddr_in *addr = (struct sockaddr_in*)ai_ptr->ai_addr;
+        fprintf(stdout, "%d %d %d %d %s\n",
+                ai_ptr->ai_flags,
+                ai_ptr->ai_family,
+                ai_ptr->ai_socktype,
+                ai_ptr->ai_protocol,
+                inet_ntoa(addr->sin_addr));
     }
+
+    freeaddrinfo(ai_list);
 
     return 0;
 }
